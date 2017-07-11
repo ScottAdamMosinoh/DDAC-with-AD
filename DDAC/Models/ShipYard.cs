f0127b11bd4ef2689e3750c402cfc8c674562c93
SCOTT ADAM MOSINOH <TP032304@mail.apu.edu.my>
2017-07-11 11:03:00 +0800

    Add project files.

---
+++ DDAC/Models/ShipYard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DDAC.Models
+{
+    public class ShipYard
+    {
+        public int ShipYardID { get; set; }
+        public String ShipYardName { get; set; }
+    }
+}
